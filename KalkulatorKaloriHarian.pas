65bc9c8b3fe9d7eb0f12f07ce4ced78c1709d990
christysaragih <gorachristy28@gmail.com>
2024-12-10 09:57:16 +0700

    KalkulatorKaloriHarian.pas

--- KalkulatorKaloriHarian.pas
+++ KalkulatorKaloriHarian.pas
@@ -16,4 +16,28 @@ type menuTM = record    /// record untuk metode menu
     mamiTM : char;
     jumlahmamiTM,kaloriTM : integer;
 end;
+var 
+nama, ket0: string;
+jeniskelamin, menu, bumilsui, LanjutPesan : char;
+PilihanMetode, aktivitas, Jumlah, i, jumlahKonsumsi, bb, tb, Keterangan, umur  : integer;
+aktv, ket,bmrP, bmrL, bmrMILSUI, TotalKaloriKonsumsi, KaloriAkhir, diet, totaldiet, naikbb, totalnaikbb : real;
+data1 : array [1..100] of mandiriM;
+data2 : array [1..100] of menuTM;
+
+
+procedure welcome;
+begin
+    clrscr;
+    writeln('---------------------------------------------------');
+    writeln('Selamat Datang di Kalkulator Kalori Harian ^^');
+    writeln('---------------------------------------------------');
+    writeln('Ingin menghitung kalori dengan metode apa hari ini?');
+    writeln('Pilihan : ');
+    writeln('   1. Mandiri ');
+    writeln('   2. Tampilan Menu');
+    writeln('-------------------------------------------------');
+    writeln;
+    write('Pilih Metode (1/2) : ');
+    readln(PilihanMetode);
+end;
 
